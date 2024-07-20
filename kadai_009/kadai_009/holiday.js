9de3f81882033f09704c2e5f6ce301b768ca4ffe
モチヅキミキ <tonton0312tonton@gmail.com>
2024-07-20 23:38:50 +0900

    kadai_009を修正

---
+++ kadai_009/kadai_009/holiday.js
@@ -0,0 +1,29 @@
+// 配列の宣言と値の代入を行う
+const Holiday = [
+  "正月",
+  "成人の日",
+  "建国記念日",
+  "天皇誕生日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+  "海の日",
+  "山の日",
+  "敬老の日",
+  "秋分の日",
+  "スポーツの日",
+  "文化の日",
+  "勤労感謝の日",
+];
+for (let i = 0; i < Holiday.length; i++) {
+  console.log(Holiday[i]);
+}
+
+let count = 0;
+
+while (count < Holiday.length) {
+  console.log(Holiday[count]);
+  count++;
+}
